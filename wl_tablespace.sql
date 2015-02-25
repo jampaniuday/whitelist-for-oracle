823827236a93f4c3d76df479659eb90aa8895a61
pmdba <pmdba@users.noreply.github.com>
2015-02-25 18:26:09 -0500

    Update wl_tablespace.sql

--- wl_tablespace.sql
+++ wl_tablespace.sql
@@ -1,3 +1,19 @@
+/****************************************************************************
+* NAME:     WL_TABLESPACE.SQL
+*
+* PURPOSE:  This script creates the tablespace which contains whitelist 
+* database objects. It requires the tablespace name for the whitelist
+* as input.
+*
+* INPUTS:    &1 = Tablespace Name
+*
+* REVISIONS:
+* Ver        Date        Author           Description
+* ---------  ----------  ---------------  ---------------------------------
+* 1.0        10/19/2014  pmdba            1. Created this script.
+*
+****************************************************************************/
+
 CREATE SMALLFILE TABLESPACE &1     
 DATAFILE SIZE 102400 K AUTOEXTEND ON NEXT 102400 K MAXSIZE UNLIMITED     
 LOGGING     
