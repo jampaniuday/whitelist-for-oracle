a75a67e47bf01da06bbf3f4ba2369070df6f7342
pmdba <pmdba@users.noreply.github.com>
2015-02-25 18:27:37 -0500

    Update wl_user.sql

--- wl_user.sql
+++ wl_user.sql
@@ -1,9 +1,30 @@
+/****************************************************************************
+* NAME:     WL_USER.SQL
+*
+* PURPOSE:  This script creates the schema which contains whitelist database
+* objects. It requires the schema name and tablespace name for the whitelist
+* as inputs.
+*
+* INPUTS:    &1 = Schema Name
+*            &2 = Tablespace Name
+*
+* REVISIONS:
+* Ver        Date        Author           Description
+* ---------  ----------  ---------------  ---------------------------------
+* 1.0        10/19/2014  pmdba            1. Created this script.
+*
+****************************************************************************/
+
+/* Create the user/schema for the whitelist */
+
 CREATE USER &1
 IDENTIFIED BY VALUES 'no_login_allowed'
 DEFAULT TABLESPACE &2
 QUOTA UNLIMITED ON &2
 PROFILE DEFAULT
 ACCOUNT UNLOCK;
+
+/* Grant appropriate system privileges to the whitelist user */
 
 GRANT CREATE INDEXTYPE,
       CREATE VIEW,
